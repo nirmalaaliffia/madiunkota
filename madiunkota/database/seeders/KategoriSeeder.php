3b5c00f59847a1a8e2536ff81953a7a7364abf66
nirmalaaliffia <nirmala.aliffia@gmail.com>
2021-11-23 17:51:57 +0700

    update model kategori

--- madiunkota/database/seeders/KategoriSeeder.php
+++ madiunkota/database/seeders/KategoriSeeder.php
@@ -16,42 +16,42 @@ class KategoriSeeder extends Seeder
     {
 
         
-        \App\Models\Kategori::create([
+        \app\Models\Kategori::create([
             'jenis_kategori' => 'Berita','warna_label' => 'Red','status_aktif' => '1'
         ]);
 
-        \App\Models\Kategori::create([
+        \app\Models\Kategori::create([
             'jenis_kategori' => 'Tabloid', 'warna_label' => 'Blue','status_aktif' => '1'
         ]);
-        \App\Models\Kategori::create([
+        \app\Models\Kategori::create([
             'jenis_kategori' => 'Pengumuman', 'warna_label' => 'Green','status_aktif' => '1'
         ]);
-        \App\Models\Kategori::create([
+        \app\Models\Kategori::create([
             'jenis_kategori' => 'lowongan', 'warna_label' => 'Brown','status_aktif' => '1'
         ]);
         
-        \App\Models\Kategori::create([
+        \app\Models\Kategori::create([
             'jenis_kategori' => 'Arsip', 'warna_label' => 'Yellow','status_aktif' => '1'
         ]);
-        \App\Models\Kategori::create([
+        \app\Models\Kategori::create([
             'jenis_kategori' => 'Dokumen', 'warna_label' => 'purple','status_aktif' => '1'
         ]);
-        \App\Models\Kategori::create([
+        \app\Models\Kategori::create([
             'jenis_kategori' => 'Informasi', 'warna_label' => 'teal','status_aktif' => '1'
         ]);
-        \App\Models\Kategori::create([
+        \app\Models\Kategori::create([
             'jenis_kategori' => 'Kegiatan Pemkot', 'warna_label' => 'fuschia','status_aktif' => '1'
         ]);
-        \App\Models\Kategori::create([
+        \app\Models\Kategori::create([
             'jenis_kategori' => 'Kegiatan Walikota', 'warna_label' => 'navy','status_aktif' => '1'
         ]);
-        \App\Models\Kategori::create([
+        \app\Models\Kategori::create([
             'jenis_kategori' => 'Data Penduduk 2019', 'warna_label' => 'lime','status_aktif' => '1'
         ]);
-        \App\Models\Kategori::create([
+        \app\Models\Kategori::create([
             'jenis_kategori' => 'Foto', 'warna_label' => 'lime','status_aktif' => '1'
         ]);
-        \App\Models\Kategori::create([
+        \app\Models\Kategori::create([
             'jenis_kategori' => 'Video', 'warna_label' => 'black','status_aktif' => '1'
         ]);
   
