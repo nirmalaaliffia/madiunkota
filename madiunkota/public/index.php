d0a8fcde1b21eeca7e45b8e00352ebb1ffb6337e
nirmalaaliffia <nirmala.aliffia@gmail.com>
2021-11-24 10:32:57 +0700

    coba

--- madiunkota/public/index.php
+++ madiunkota/public/index.php
@@ -31,8 +31,8 @@ if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
-
+// require __DIR__.'/../vendor/autoload.php';
+require_once(__DIR__ . '/vendor/autoload.php');
 /*
 |--------------------------------------------------------------------------
 | Run The Application
