a00786686a65643e4475a940a4e32ebb07cc583d
nirmalaaliffia <nirmala.aliffia@gmail.com>
2021-11-24 10:00:48 +0700

    coba

--- madiunkota/resources/views/layouts/admin.blade.php
+++ madiunkota/resources/views/layouts/admin.blade.php
@@ -11,8 +11,8 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{ asset('template_admin/plugins/fontawesome-free/css/all.min.css') }}">
-  {{-- <!-- Ionicons -->
+  {{-- <link rel="stylesheet" href="{{ asset('template_admin/plugins/fontawesome-free/css/all.min.css') }}"> --}}
+  <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Tempusdominus Bootstrap 4 -->
   <link rel="stylesheet" href="{{ asset('template_admin/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
@@ -27,7 +27,7 @@
   <!-- Daterange picker -->
   <link rel="stylesheet" href="{{ asset('template_admin/plugins/daterangepicker/daterangepicker.css') }}">
   <!-- summernote -->
-  <link rel="stylesheet" href="{{ asset('template_admin/plugins/summernote/summernote-bs4.min.css') }}"> --}}
+  <link rel="stylesheet" href="{{ asset('template_admin/plugins/summernote/summernote-bs4.min.css') }}">
 
     <!-- Select2 -->
     <link rel="stylesheet" href="{{ asset('template_admin/plugins/select2/css/select2.min.css') }}">
