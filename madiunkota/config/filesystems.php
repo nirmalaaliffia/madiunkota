68e827d269a8ae5f9085858162aa2dcdfe026b21
nirmalaaliffia <nirmala.aliffia@gmail.com>
2021-11-24 09:52:47 +0700

    coba

--- madiunkota/config/filesystems.php
+++ madiunkota/config/filesystems.php
@@ -17,19 +17,6 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Default Cloud Filesystem Disk
-    |--------------------------------------------------------------------------
-    |
-    | Many applications store files both locally and in the cloud. For this
-    | reason, you may specify a default "cloud" driver here. This driver
-    | will be bound as the Cloud disk implementation in the container.
-    |
-    */
-
-    'cloud' => env('FILESYSTEM_CLOUD', 's3'),
-
-    /*
-    |--------------------------------------------------------------------------
     | Filesystem Disks
     |--------------------------------------------------------------------------
     |
@@ -45,8 +32,7 @@ return [
 
         'local' => [
             'driver' => 'local',
-            // 'root' => storage_path('app'),
-            'root' => public_path(),
+            'root' => storage_path('app'),
         ],
 
         'public' => [
@@ -64,6 +50,7 @@ return [
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
         ],
 
     ],
