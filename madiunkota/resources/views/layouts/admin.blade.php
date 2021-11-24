ac1e5ebd66d62f38270450299e8f487597ce9b0e
nirmalaaliffia <nirmala.aliffia@gmail.com>
2021-11-24 10:00:26 +0700

    coba

--- madiunkota/resources/views/layouts/admin.blade.php
+++ madiunkota/resources/views/layouts/admin.blade.php
@@ -10,9 +10,9 @@
   <link href="{{ asset('assets/img/Lambang_Kota_Madiun.png') }}" rel="apple-touch-icon">
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  {{-- <!-- Font Awesome -->
+  <!-- Font Awesome -->
   <link rel="stylesheet" href="{{ asset('template_admin/plugins/fontawesome-free/css/all.min.css') }}">
-  <!-- Ionicons -->
+  {{-- <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Tempusdominus Bootstrap 4 -->
   <link rel="stylesheet" href="{{ asset('template_admin/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
