684d8ff47cb92cddf45c7acffff452869c2e9475
nirmalaaliffia <nirmala.aliffia@gmail.com>
2021-11-25 15:19:22 +0700

    update landing

--- madiunkota/resources/views/home.blade.php
+++ madiunkota/resources/views/home.blade.php
@@ -709,7 +709,7 @@ for ($i=0; $i < 3; $i++) { ?>
 @endsection
 @push('script')
 <script>
-    var url = 'https://wati-integration-service.clare.ai/ShopifyWidget/shopifyWidget.js?1378';
+    var url = 'https://wati-integration-service.clare.ai/ShopifyWidget/shopifyWidget.js?35028';
     var s = document.createElement('script');
     s.type = 'text/javascript';
     s.async = true;
@@ -722,7 +722,7 @@ for ($i=0; $i < 3; $i++) { ?>
       "borderRadius":"25",
       "marginLeft":"50",
       "marginBottom":"50",
-      "marginRight":"150",
+      "marginRight":"100",
       "position":"right"
   },
   "brandSetting":{
